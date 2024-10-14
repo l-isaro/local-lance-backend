77519119c856021d16db20df3c26d412f44c6589
l-isaro <l.isaro@alustudent.com>
2024-10-14 11:27:46 +0200

    fix: fix jwt secret accessibility

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,12 +4,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './schemas/user.schema';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' }, // or any desired expiration time
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [UserService],
